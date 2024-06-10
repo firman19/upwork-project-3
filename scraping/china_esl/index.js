f917ec837bf9dd0b042994e47be08b40c1a51642
Adi Firman <firmanilham19@gmail.com>
2024-06-10 22:05:12 +0700

    Add an email

---
+++ scraping/china_esl/index.js
@@ -0,0 +1,10 @@
+const jobListEl = `div#job-listings > div.row`;
+const titleEl = `div#job-listings > div.row > span.row-info > a`; //text
+const jobTypeEl = `div#job-listings > div.row > span.row-info > img`; //src
+const linknEl = `div#job-listings > div.row > span.row-info > a`; //href
+const timeEl = `div#job-listings > div.row > span.time-posted`;
+
+// detail
+const descEl = `div#job-description`
+const business_name_El = `div#job-details>p`
+
