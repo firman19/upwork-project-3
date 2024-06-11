5b8025bd8b6247dfbf43451e0da98e47622c733d
Adi Firman <firmanilham19@gmail.com>
2024-06-11 21:08:13 +0700

    Init scraping chinaesl

---
+++ scraping/china_esl/detail.js
@@ -0,0 +1,59 @@
+import puppeteer from "puppeteer";
+import { writeFile } from "fs/promises";
+import { parse } from "json2csv";
+import tmp from "./results/chinaesl_company_agencies_tmp.json" with { type: "json" }
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+  console.log("Length: " + tmp.length);
+
+  for (let i = 0; i < tmp.length; i++) {
+    let company_url = tmp[i].company_url;
+    console.log("Process:" + i + "/" + tmp.length, company_url);
+    if (company_url) {
+      try {
+        await page.goto(company_url, {
+          waitUntil: "domcontentloaded",
+        });
+
+        let jobDescElement = (await page.$(`div#job-description`)) || "";
+        let businessNameElement = (await page.$(`div#job-details>p`)) || "";
+        let job_desc = jobDescElement
+          ? await jobDescElement.evaluate((el) => el.textContent)
+          : "";
+        let business_name = businessNameElement
+          ? await businessNameElement.evaluate((el) => el.textContent)
+          : "";
+        
+          business_name = business_name.replace(/(\r\n|\n|\r|\t)/gm, "");
+          business_name = business_name.startsWith('at') ? business_name.substring(2) : business_name;
+
+        tmp[i] = {
+          ...tmp[i],
+          job_desc: job_desc.replace(/(\r\n|\n|\r|\t)/gm, "").replace('\t',''),
+          business_name: business_name.trim(),
+        };
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+    console.log("Saving json...");
+    await writeFile(
+      `results/china_esl_company_agency_full.json`,
+      JSON.stringify(tmp, null, 2)
+    );
+
+    console.log("Saving csv...");
+    const csv = parse(tmp);
+    await writeFile(`results/china_esl_company_agency_full.csv`, csv);
+    await browser.close();
+};
+
+main();
--- scraping/china_esl/index.js
+++ scraping/china_esl/index.js
@@ -1,3 +1,6 @@
+import puppeteer from "puppeteer";
+import { writeFile } from "fs/promises";
+
 const jobListEl = `div#job-listings > div.row`;
 const titleEl = `div#job-listings > div.row > span.row-info > a`; //text
 const jobTypeEl = `div#job-listings > div.row > span.row-info > img`; //src
@@ -5,6 +8,100 @@ const linknEl = `div#job-listings > div.row > span.row-info > a`; //href
 const timeEl = `div#job-listings > div.row > span.time-posted`;
 
 // detail
-const descEl = `div#job-description`
-const business_name_El = `div#job-details>p`
+const descEl = `div#job-description`;
+const business_name_El = `div#job-details>p`;
 
+// company agencies page 1
+let URL = "http://www.chinaesljob.com/jobs/Company-Agencies/?p=1";
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(URL, {
+    waitUntil: "load",
+  });
+
+  // wait for the job list to display
+  await page.waitForFunction(
+    () => document.querySelectorAll(`div#job-listings > div.row`).length
+  );
+  console.log("joblist has been displayed");
+
+  const jobs_arr = [];
+  let start = 1;
+  let totalJob = 0;
+
+  // count how many total html element jobs
+  totalJob = await page.evaluate(() => {
+    const jobElements = document.querySelectorAll(`div#job-listings > div`);
+    return jobElements.length;
+  });
+  console.log("total jobs", totalJob);
+
+  for (let i = start; i <= totalJob; i++) {
+    let titleElement =
+      (await page.$(
+        `div#job-listings > div:nth-child(${i}) span.row-info > a`
+      )) || "";
+    let jobTypeElement =
+      (await page.$(
+        `div#job-listings > div:nth-child(${i}) span.row-info > img`
+      )) || "";
+    let timeElement =
+      (await page.$(
+        `div#job-listings > div:nth-child(${i}) span.time-posted`
+      )) || "";
+
+    /** =========== */
+    let title = titleElement
+      ? await titleElement.evaluate((el) => el.textContent)
+      : "";
+
+    let job_type = jobTypeElement
+      ? await jobTypeElement.evaluate((el) => el.getAttribute("src"))
+      : "";
+
+    let link = titleElement
+      ? await titleElement.evaluate((el) => el.getAttribute("href"))
+      : "";
+
+    let time = timeElement
+      ? await timeElement.evaluate((el) => el.textContent)
+      : "";
+
+    jobs_arr.push({
+      post_date: time.replace(/\n/g, "").trim(),
+      job_title: title.replace(/\n/g, "").trim(),
+      job_type: getJobType(job_type),
+      company_url: link,
+      //   salary: salary.replace(/\n/g, "").trim(),
+      //   city: city.replace(/\n/g, "").trim(),
+      //   business_name: company,
+      //   featured: featured ? "YES" : "NO",
+    });
+  }
+
+  console.log(jobs_arr);
+  console.log("Saving json...");
+  await writeFile(
+    `results/chinaesl_company_agencies_tmp.json`,
+    JSON.stringify(jobs_arr, null, 2)
+  );
+  await browser.close();
+};
+
+main();
+
+function getJobType(str) {
+  if (str.includes("fulltime")) {
+    return "Full Time";
+  }
+  if (str.includes("parttime")) {
+    return "Part Time";
+  }
+  return "";
+}
