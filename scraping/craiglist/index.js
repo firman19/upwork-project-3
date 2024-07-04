386061dd76c2f22c16be36cea4fe1597c9a17ca6
Adi Firman <firmanilham19@gmail.com>
2024-07-04 09:00:06 +0700

    Init list craiglist

--- scraping/craiglist/index.js
+++ scraping/craiglist/index.js
@@ -29,7 +29,10 @@ const main = async () => {
 
       // wait for the job list to display
       await page.waitForFunction(
-        () => document.querySelectorAll(`div.results.cl-results-page li.cl-search-result`).length
+        () =>
+          document.querySelectorAll(
+            `div.results.cl-results-page li.cl-search-result`
+          ).length
       );
       console.log("joblist has been displayed");
     } catch (error) {
@@ -50,22 +53,100 @@ const main = async () => {
     let start = 1;
     for (let i = start; i <= totalJob; i++) {
       try {
+        /** Define elements */
+        let locationElement =
+          (await page.$(
+            `div.results.cl-results-page li.cl-search-result:nth-child(${i})
+            div.supertitle`
+          )) || "";
+        let jobLinkElement =
+          (await page.$(
+            `div.results.cl-results-page li.cl-search-result:nth-child(${i})
+            a.posting-title`
+          )) || "";
+        let dateElement =
+          (await page.$(
+            `div.results.cl-results-page li.cl-search-result:nth-child(${i})
+            div.meta span`
+          )) || "";
+        let salaryElement =
+          (await page.$(
+            `div.results.cl-results-page li.cl-search-result:nth-child(${i})
+            div.meta span:nth-child(2)`
+          )) || "";
+
+        /** Retrieve values */
+        let location = locationElement
+          ? await locationElement.evaluate((el) => el.textContent)
+          : "";
+        let job_link = jobLinkElement
+          ? await jobLinkElement.evaluate((el) => el.getAttribute("href"))
+          : "";
+        let title = jobLinkElement
+          ? await jobLinkElement.evaluate((el) => el.textContent)
+          : "";
+        let date = dateElement
+          ? await dateElement.evaluate((el) => el.getAttribute("title"))
+          : "";
+        // TODO
+        let salary = salaryElement
+          ? await salaryElement.evaluate((el) => el.textContent)
+          : "";
+
+        jobs_arr.push({
+          date_farmed: "04/07/2024",
+          source: "CraigList",
+          scraped_url: job_link,
+          d: "New Lead",
+          e: "Opportunity",
+          f: "Juan",
+          g: "Juan",
+          h: "Lara",
+          i: "Scraping",
+          j: "EDU Business",
+          k: "EDU Jobs",
+          l: "",
+          post_date: date,
+          n: "", // TODO: get feat job
+          job_title: title.replace(/\n/g, "").trim(),
+          p: "",
+          job_type: "",
+          description: "",
+          s: "",
+          t: "",
+          salary: salary,
+          v: "",
+          w_requirements: "",
+          x_benefits: "",
+          y: "",
+          z: "",
+          aa_state_province: location.replace(/\n/g, "").trim(),
+          ab_country: "USA",
+          ac_region: "North America",
+          business_name: "",
+          ae_description: "",
+          af_website: "",
+          ag: "",
+          ah: "",
+          ai_country: "",
+          company_url: "", // to get website
+        });
       } catch (error) {
         console.error(error);
       }
     }
   }
 
-  // console.log("Saving json...");
-  // await writeFile(
-  //   `results/craiglist_tmp.json`,
-  //   JSON.stringify(jobs_arr, null, 2)
-  // );
+  console.log("Saving json...");
+  await writeFile(
+    `results/craiglist_tmp.json`,
+    JSON.stringify(jobs_arr, null, 2)
+  );
 
-  // console.log("Saving csv...");
-  // const csv = parse(jobs_arr);
-  // await writeFile(`results/craiglist_tmp.csv`, csv);
-  // await browser.close();
+  console.log("Saving csv...");
+  const csv = parse(jobs_arr);
+  await writeFile(`results/craiglist_tmp.csv`, csv);
+  await browser.close();
 };
 
 main();
