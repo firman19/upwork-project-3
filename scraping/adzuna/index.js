78f42211998994a1259275de45d368493155c1f4
Adi Firman <firmanilham19@gmail.com>
2024-07-04 08:38:30 +0700

    Init list Adzuna

--- scraping/adzuna/index.js
+++ scraping/adzuna/index.js
@@ -50,19 +50,88 @@ const main = async () => {
     let start = 1;
     for (let i = start; i <= totalJob; i++) {
       try {
+        /** Define elements */
+        let jobLinkElement =
+          (await page.$(
+            `div.ui-search-results>div[data-aid]:nth-child(${i}) h2 a`
+          )) || "";
+        let businessLinkElement =
+          (await page.$(
+            `div.ui-search-results>div[data-aid]:nth-child(${i}) div.ui-company a`
+          )) || "";
+        let locationElement =
+          (await page.$(
+            `div.ui-search-results>div[data-aid]:nth-child(${i}) div.ui-location`
+          )) || "";
+
+        /** Retrieve values */
+        let job_link = jobLinkElement
+          ? await jobLinkElement.evaluate((el) => el.getAttribute("href"))
+          : "";
+        let title = jobLinkElement
+          ? await jobLinkElement.evaluate((el) => el.textContent)
+          : "";
+        let company_url = businessLinkElement
+          ? await businessLinkElement.evaluate((el) => el.getAttribute("href"))
+          : "";
+        let business_name = businessLinkElement
+          ? await businessLinkElement.evaluate((el) => el.textContent)
+          : "";
+        let location = locationElement
+          ? await locationElement.evaluate((el) => el.textContent)
+          : "";
+
+        jobs_arr.push({
+          date_farmed: "04/07/2024",
+          source: "Adzuna",
+          scraped_url: job_link,
+          d: "New Lead",
+          e: "Opportunity",
+          f: "Juan",
+          g: "Juan",
+          h: "Lara",
+          i: "Scraping",
+          j: "EDU Business",
+          k: "EDU Jobs",
+          l: "",
+          post_date: "",
+          n: "", // TODO: get feat job
+          job_title: title.replace(/\n/g, "").trim(),
+          p: "",
+          job_type: "",
+          description: "",
+          s: "",
+          t: "",
+          salary: "",
+          v: "",
+          w_requirements: "",
+          x_benefits: "",
+          y: "",
+          z: "",
+          aa_state_province: location.replace(/\n/g, "").trim(),
+          ab_country: "Australia",
+          ac_region: "Oceania",
+          business_name: business_name,
+          ae_description: "",
+          af_website: "",
+          ag: "",
+          ah: "",
+          ai_country: "",
+          company_url: company_url, // to get website
+        });
       } catch (error) {
         console.error(error);
       }
     }
   }
 
-  // console.log("Saving json...");
-  // await writeFile(`results/adzuna_tmp.json`, JSON.stringify(jobs_arr, null, 2));
+  console.log("Saving json...");
+  await writeFile(`results/adzuna_tmp.json`, JSON.stringify(jobs_arr, null, 2));
 
-  // console.log("Saving csv...");
-  // const csv = parse(jobs_arr);
-  // await writeFile(`results/adzuna_tmp.csv`, csv);
-  // await browser.close();
+  console.log("Saving csv...");
+  const csv = parse(jobs_arr);
+  await writeFile(`results/adzuna_tmp.csv`, csv);
+  await browser.close();
 };
 
 main();
