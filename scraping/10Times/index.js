169ba93b3ac6470b8a64cbabe5878f619db9e3df
Adi Firman <firmanilham19@gmail.com>
2024-07-11 13:39:22 +0700

    added 2 emails

--- scraping/10Times/index.js
+++ scraping/10Times/index.js
@@ -1,9 +1,9 @@
-// docs [INSERT_GOOGLE_DOC_HERE]
+// docs [https://docs.google.com/document/d/1QMkfJKgeYMZizQSytuc2SSnIMI_ai4ZM/edit]
 import puppeteer from "puppeteer";
 import { writeFile } from "fs/promises";
 import { parse } from "json2csv";
 
-const URL = `[INSERT_URL_HERE]`;
+const URL = `https://10times.com/indonesia?kw=Education`;
 // const URL = "https://www.adzuna.com.au/search?loc=105392&q=teaching&page=";
 
 const main = async () => {
@@ -16,13 +16,13 @@ const main = async () => {
 
   // handle pagination
   let counter = 1;
-  let lastPage = 20;
+  let lastPage = 1;
   const jobs_arr = [];
 
   while (counter <= lastPage) {
     console.log("page: " + counter);
     try {
-      const pageURL = URL + counter;
+      const pageURL = URL;
       counter = counter + 1;
       await page.goto(pageURL, {
         waitUntil: "load",
@@ -30,7 +30,7 @@ const main = async () => {
 
       // wait for the job list to display
       await page.waitForFunction(
-        () => document.querySelectorAll(`[INSERT_SELECTOR_JOB_LIST]`).length
+        () => document.querySelectorAll(`table#listing-events`).length
         // () => document.querySelectorAll(`div.ui-search-results`).length
       );
       console.log("joblist has been displayed");
@@ -43,7 +43,7 @@ const main = async () => {
     let totalJob = 0;
     totalJob = await page.evaluate(() => {
       const jobElements = document.querySelectorAll(
-        `[INSERT_SELECTOR_JOB_ITEM]`
+        `table#listing-events tr`
         // `div.ui-search-results>div[data-aid]`
       );
       return jobElements.length;
@@ -54,24 +54,34 @@ const main = async () => {
     for (let i = start; i <= totalJob; i++) {
       try {
         /** Define elements */
-        let jobLinkElement =
+        let location_Element =
+          (await page.$(`table#listing-events tr:nth-child(${i}) .venue`)) ||
+          "";
+        let job_titleElement =
+          (await page.$(`table#listing-events tr:nth-child(${i}) td h2 a`)) ||
+          "";
+        let dateElement =
           (await page.$(
-            `div.results.cl-results-page li[data-pid].cl-search-result:nth-child(${i}) a.posting-title`
-            // `div.results.cl-results-page li[data-pid].cl-search-result:nth-child(${i}) a.posting-title`
+            `table#listing-events tr:nth-child(${i})>td>div.small.fw-500`
           )) || "";
 
         /** Retrieve values */
-        let job_link = jobLinkElement
-          ? await jobLinkElement.evaluate((el) => el.textContent)
+        let job_title = job_titleElement
+          ? await job_titleElement.evaluate((el) => el.textContent)
           : "";
+        let location = location_Element
+          ? await location_Element.evaluate((el) => el.textContent)
+          : "";
+        let date = dateElement
+          ? await dateElement.evaluate((el) => el.textContent)
+          : "";
+
+        console.log(date);
 
         jobs_arr.push({
           date_farmed: "04/07/2024",
           source: "",
-          scraped_url: job_link
-            .replace(/(\r\n|\n|\r|\t)/gm, "")
-            .replace("\t", "")
-            .trim(),
+          scraped_url: job_title,
           d: "New Lead",
           e: "Opportunity",
           f: "Juan",
@@ -89,7 +99,7 @@ const main = async () => {
           description: "",
           s: "",
           t: "",
-          salary: salary,
+          salary: "",
           v: "",
           w_requirements: "",
           x_benefits: "",
@@ -111,17 +121,16 @@ const main = async () => {
       }
     }
   }
+  // console.log("Saving json...");
+  // await writeFile(
+  //   `results/10Times_tmp.json`,
+  //   JSON.stringify(jobs_arr, null, 2)
+  // );
 
-  console.log("Saving json...");
-  await writeFile(
-    `results/10Times_tmp.json`,
-    JSON.stringify(jobs_arr, null, 2)
-  );
-
-  console.log("Saving csv...");
-  const csv = parse(jobs_arr);
-  await writeFile(`results/10Times_tmp.csv`, csv);
-  await browser.close();
+  // console.log("Saving csv...");
+  // const csv = parse(jobs_arr);
+  // await writeFile(`results/10Times_tmp.csv`, csv);
+  // await browser.close();
 };
 
 main();
