7b788a4bdc7127a8ec6b3a8ef24b0fc18e2a3ba6
Adi Firman <firmanilham19@gmail.com>
2024-07-12 17:56:53 +0700

    scrape 10Times

--- scraping/10Times/details.js
+++ scraping/10Times/details.js
@@ -2,56 +2,118 @@ import puppeteer from "puppeteer";
 import { writeFile } from "fs/promises";
 import { parse } from "json2csv";
 
-import tmp from "./results/10Times_tmp.json" with { type: "json" }
+import tmp from "./results/10Times_tmp_australia.json" with { type: "json" }
 
 const main = async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-
-  const page = await browser.newPage();
   console.log("Length: " + tmp.length);
 
   for (let i = 0; i < tmp.length; i++) {
-    let scraped_url = tmp[i].scraped_url;
-    let company_url = tmp[i].company_url;
-    console.log("Process:" + i + "/" + tmp.length);
+    let browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+    });
 
+    let page = await browser.newPage();
+    let scraped_url = tmp[i].c_scraped_url;
+    console.log("Process:" + i + "/" + tmp.length);
+    
+    let companyUrl = "";
     if (scraped_url) {
       try {
         console.log("Opening scraped_url");
         await page.goto(scraped_url, {
+          timeout: 3000,
           waitUntil: "domcontentloaded",
         });
 
         /** Define elements */
-        let jobDescElement = (await page.$(`[INSERT_SELECTOR_HERE]]`)) || "";
+        let jobDescElement = (await page.$(`section.box.fs-14>p`)) || "";
+        let locElement = (await page.$(`div.mt-1.text-muted.m-mins_lft`)) || "";
+        let businessNameElement =
+          (await page.$(`#organzr strong.fs-16 a`)) || "";
 
         /**Extract from elements */
         let job_desc = jobDescElement
           ? await jobDescElement.evaluate((el) => el.textContent)
           : "";
+        let location = locElement
+          ? await locElement.evaluate((el) => el.textContent)
+          : "";
+        let businessName = businessNameElement
+          ? await businessNameElement.evaluate((el) => el.textContent)
+          : "";
+        companyUrl = businessNameElement
+          ? await businessNameElement.evaluate((el) => el.getAttribute("href"))
+          : "";
 
         tmp[i] = {
           ...tmp[i],
-          description: job_desc.replace(/(\r\n|\n|\r|\t)/gm, "")
-          .replace("\t", "")
-          .trim(),
+          o_description: job_desc
+            .replace(/(\r\n|\n|\r|\t)/gm, "")
+            .replace("\t", "")
+            .trim(),
+          s_state_province: location,
+          ab_business_info: businessName,
+          company_url: companyUrl,
         };
       } catch (error) {
         console.log(error);
       }
     }
+    await browser.close();
+    await timeout(3000);
+
+    if (companyUrl) {
+      try {
+        console.log("Opening companyUrl");
+        browser = await puppeteer.launch({
+          headless: false,
+          defaultViewport: null,
+        });
+
+        page = await browser.newPage();
+        await page.goto(companyUrl, {
+          timeout: 3000,
+          waitUntil: "domcontentloaded",
+        });
+
+        /** Define elements */
+        let websiteElement = (await page.$(`a.btn.btn-lg.btn-dark.me-3`)) || "";
+        let countryElement = (await page.$(`a.text-white.text-decoration-none`)) || "";
+
+        /**Extract from elements */
+        let website = websiteElement
+          ? await websiteElement.evaluate((el) => el.getAttribute("href"))
+          : "";
+
+        let country = countryElement
+          ? await countryElement.evaluate((el) => el.textContent)
+          : "";
+
+        console.log("country");
+        console.log(country);
+
+        tmp[i] = {
+          ...tmp[i],
+          ac_website: website,
+          af_country: country,
+        };
+      } catch (error) {}
+      await browser.close();
+      await timeout(3000);
+    }
+
+    console.log("Saving json...");
+    await writeFile(`results/10Times_full_australia.json`, JSON.stringify(tmp, null, 2));
+
+    console.log("Saving csv...");
+    const csv = parse(tmp);
+    await writeFile(`results/10Times_full_australia.csv`, csv);
   }
-
-  console.log("Saving json...");
-  await writeFile(`results/10Times_full.json`, JSON.stringify(tmp, null, 2));
-
-  console.log("Saving csv...");
-  const csv = parse(tmp);
-  await writeFile(`results/10Times_full.csv`, csv);
-  await browser.close();
 };
 
 main();
+
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
--- scraping/10Times/index.js
+++ scraping/10Times/index.js
@@ -1,10 +1,35 @@
+// docs [https://drive.google.com/drive/folders/1IEnoEqyTjMpuw-OJHpcmBuxQsAeHPrhk]
 // docs [https://docs.google.com/document/d/1QMkfJKgeYMZizQSytuc2SSnIMI_ai4ZM/edit]
 import puppeteer from "puppeteer";
 import { writeFile } from "fs/promises";
 import { parse } from "json2csv";
 
-const URL = `https://10times.com/indonesia?kw=Education`;
-// const URL = "https://www.adzuna.com.au/search?loc=105392&q=teaching&page=";
+const countries = [
+  "argentina",
+  "australia",
+  "austria",
+  "belgium",
+  "brazil",
+  "canada",
+  "chile",
+  "china",
+  "denmark",
+  "ecuador",
+
+  "egypt",
+  "france",
+  "germany",
+  "greece",
+  "india",
+  "indonesia",
+  "ireland",
+  "italy",
+  "mexico",
+  "netherlands",
+];
+const currCountry = countries[9];
+
+const URL = `https://10times.com/${currCountry}?kw=Education`;
 
 const main = async () => {
   const browser = await puppeteer.launch({
@@ -13,6 +38,26 @@ const main = async () => {
   });
 
   const page = await browser.newPage();
+
+  // Function to scroll down the page
+  async function autoScroll(page) {
+    await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+        var totalHeight = 0;
+        var distance = 1000;
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, 1000);
+      });
+    });
+  }
 
   // handle pagination
   let counter = 1;
@@ -39,13 +84,27 @@ const main = async () => {
       continue;
     }
 
+    let previousHeight;
+    let currentHeight = await page.evaluate(() => document.body.scrollHeight);
+    let maxScrolls = 20; // Limit to avoid infinite loops
+    let scrollCount = 0;
+
+    while (scrollCount < maxScrolls) {
+      previousHeight = currentHeight;
+      await autoScroll(page);
+      // await page.waitForTimeout(2000); // Adjust timeout as needed
+      currentHeight = await page.evaluate(() => document.body.scrollHeight);
+
+      if (previousHeight === currentHeight) {
+        break; // Break if no new content is loaded
+      }
+      scrollCount++;
+    }
+
     // count how many total html element jobs
     let totalJob = 0;
     totalJob = await page.evaluate(() => {
-      const jobElements = document.querySelectorAll(
-        `table#listing-events tr`
-        // `div.ui-search-results>div[data-aid]`
-      );
+      const jobElements = document.querySelectorAll(`table#listing-events tr`);
       return jobElements.length;
     });
     console.log("total jobs", totalJob);
@@ -54,20 +113,23 @@ const main = async () => {
     for (let i = start; i <= totalJob; i++) {
       try {
         /** Define elements */
-        let location_Element =
-          (await page.$(`table#listing-events tr:nth-child(${i}) .venue`)) ||
-          "";
-        let job_titleElement =
-          (await page.$(`table#listing-events tr:nth-child(${i}) td h2 a`)) ||
-          "";
         let dateElement =
           (await page.$(
             `table#listing-events tr:nth-child(${i})>td>div.small.fw-500`
           )) || "";
+        let job_titleElement =
+          (await page.$(`table#listing-events tr:nth-child(${i}) td h2 a`)) ||
+          "";
+        let location_Element =
+          (await page.$(`table#listing-events tr:nth-child(${i}) .venue`)) ||
+          "";
 
         /** Retrieve values */
-        let job_title = job_titleElement
+        let title = job_titleElement
           ? await job_titleElement.evaluate((el) => el.textContent)
+          : "";
+        let job_link = job_titleElement
+          ? await job_titleElement.evaluate((el) => el.getAttribute("href"))
           : "";
         let location = location_Element
           ? await location_Element.evaluate((el) => el.textContent)
@@ -76,12 +138,10 @@ const main = async () => {
           ? await dateElement.evaluate((el) => el.textContent)
           : "";
 
-        console.log(date);
-
         jobs_arr.push({
-          date_farmed: "04/07/2024",
-          source: "",
-          scraped_url: job_title,
+          a_date_farmed: "12/07/2024",
+          b_source: "10Times",
+          c_scraped_url: job_link,
           d: "New Lead",
           e: "Opportunity",
           f: "Juan",
@@ -89,48 +149,49 @@ const main = async () => {
           h: "Lara",
           i: "Scraping",
           j: "EDU Business",
-          k: "EDU Jobs",
+          k: "EDU Events",
           l: "",
-          post_date: "",
-          n: "",
-          job_title: "",
+          m_date_of_event: date,
+          n_event_name: title,
+          o_description: "",
           p: "",
-          job_type: "",
-          description: "",
-          s: "",
-          t: "",
-          salary: "",
+          //
+          q: "",
+          r: "",
+          s_state_province: location,
+          t: "Ecuador",
+          u: "South America",
+          //
           v: "",
-          w_requirements: "",
-          x_benefits: "",
+          w: "",
+          x: "",
           y: "",
           z: "",
-          aa_state_province: "",
-          ab_country: "USA",
-          ac_region: "North America",
-          business_name: "",
-          ae_description: "",
-          af_website: "",
+          aa: "",
+          //
+          ab_business_info: "",
+          ac_website: "",
+          ad: "",
+          ae: "",
+          af_country: "",
           ag: "",
-          ah: "",
-          ai_country: "",
-          company_url: "",
+          //
         });
       } catch (error) {
         console.error(error);
       }
     }
   }
-  // console.log("Saving json...");
-  // await writeFile(
-  //   `results/10Times_tmp.json`,
-  //   JSON.stringify(jobs_arr, null, 2)
-  // );
+  console.log("Saving json...");
+  await writeFile(
+    `results/10Times_tmp_${currCountry}.json`,
+    JSON.stringify(jobs_arr, null, 2)
+  );
 
-  // console.log("Saving csv...");
-  // const csv = parse(jobs_arr);
-  // await writeFile(`results/10Times_tmp.csv`, csv);
-  // await browser.close();
+  console.log("Saving csv...");
+  const csv = parse(jobs_arr);
+  await writeFile(`results/10Times_tmp_${currCountry}.csv`, csv);
+  await browser.close();
 };
 
 main();
